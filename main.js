11a557494c4ee65c9b7d56403e92178d5ea82d89
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-02-26 04:02:31 -0500

    Add pokemon-img style to disappear according to pokedex opening

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let habilidadEl = document.querySelector("#ability");
 let nombreEl = document.querySelector("#name");
 let movimientoEl = document.querySelector("#movement");
 let ordenEl = document.querySelector("#order");
-let imgEl = document.querySelector("#pokemon-img");
+let pokeImgEl = document.querySelector("#pokemon-img");
 
 let buscarRandomEl = document.querySelector("#buscarRandom");
 let buscarIdEl = document.querySelector("#buscarId");
@@ -27,7 +27,7 @@ function putData(data) {
     console.log(data.moves[random_num].move.name)
     movimientoEl.innerHTML = data.moves[random_num].move.name;
     ordenEl.innerHTML = data.id;
-    imgEl.src = data.sprites.front_default;
+    pokeImgEl.src = data.sprites.front_default;
 }
 
 async function buscarId() {
@@ -54,37 +54,22 @@ let pokeContainer = document.querySelector(".poke-container");
 let pokeLeftFlip = document.querySelector(".poke-left-flip");
 
 pokeContainer.addEventListener("click", openContainer);
-// pokeContainer.addEventListener("click", closeContainer);
-
-let isOpen = false;
-
-// function openContainer() {
-//     if (pokeContainer.classList.contains('open')) {
-//         pokeContainer.classList.add('close-pokedex');
-//         // pokeLeftFlip.classList.add("close-left-flip");
-//         // pokeContainer.classList.remove('open-pokedex', 'open-left-flip');
-//         // pokeContainer.classList.add('close-pokedex', 'close-left-flip');
-//         console.log("CONTIENE");
-//         pokeContainer.classList.remove('open');
-//     } else {
-//         pokeContainer.classList.add("open");
-//         pokeContainer.classList.add('open-pokedex');
-//         pokeLeftFlip.classList.add("open-left-flip");
-//         // pokeContainer.classList.remove('close-pokedex', 'close-left-flip');
-//     }
-
-// }
-
 
 function openContainer() {
     if (pokeContainer.classList.contains('open')) {
         pokeContainer.classList.add('close-pokedex');
         pokeLeftFlip.classList.add('close-left-flip');
         pokeContainer.classList.remove('open');
+        pokeImgEl.style.display = "none";
+
     } else {
         pokeContainer.classList.remove('close-pokedex');
         pokeLeftFlip.classList.remove('close-left-flip');
         pokeContainer.classList.add('open', 'open-pokedex');
         pokeLeftFlip.classList.add('open-left-flip');
+        setTimeout(() => {
+            pokeImgEl.style.display = "initial";
+        }, 1800);
     }
 }
+
