02e4713c1cbecde6c023c4fc6336b9be5ed55529
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-02-25 19:17:06 -0500

    Refactor and Add event listener to open the pokedex

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 
-let habilidadEl = document.querySelector("#habilidad");
-let nombreEl = document.querySelector("#nombre");
-let movimientoEl = document.querySelector("#movimiento");
-let ordenEl = document.querySelector("#orden");
-let imgEl = document.querySelector("#image");
+let habilidadEl = document.querySelector("#ability");
+let nombreEl = document.querySelector("#name");
+let movimientoEl = document.querySelector("#movement");
+let ordenEl = document.querySelector("#order");
+let imgEl = document.querySelector("#pokemon-img");
 
 let buscarRandomEl = document.querySelector("#buscarRandom");
 let buscarIdEl = document.querySelector("#buscarId");
 
 async function buscar() {
-
     let random_id = Math.floor(Math.random() * 150);
     console.log(random_id);
     let url = `https://pokeapi.co/api/v2/pokemon/${random_id}`;
@@ -19,7 +18,6 @@ async function buscar() {
     console.log(data);
     habilidad = data.abilities[1].ability.name;
     putData(data);
-
 }
 
 function putData(data) {
@@ -32,13 +30,11 @@ function putData(data) {
     imgEl.src = data.sprites.front_default;
 }
 
-
 async function buscarId() {
 
     const id = document.querySelector("#idpoke").value;
 
     if (id) {
-
         let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
         const response = await fetch(url);
         const data = await response.json();
@@ -47,13 +43,23 @@ async function buscarId() {
         console.log(data);
         habilidad = data.abilities[random_num].ability.name;
         putData(data);
-
     }
     console.log(id, "No hay id");
-
 }
 
 buscarRandomEl.addEventListener("click", buscar);
 buscarIdEl.addEventListener("click", buscarId);
 
-console.log(habilidadEl.innerHTML)
+let pokeContainer = document.querySelector(".poke-container");
+let pokeLeftFlip = document.querySelector(".poke-left-flip");
+
+pokeContainer.addEventListener("click", openContainer);
+
+let isZoom = false;
+
+function openContainer() {
+    pokeContainer.classList.add("open-pokedex");
+    pokeLeftFlip.classList.add("open-left-flip");
+}
+
+console.log(habilidadEl.innerHTML);
